b58d7027ef739cd466ef5e782b8a31fb3c12169f
NNoorFatima <noor.2205.nf@gmail.com>
2025-05-28 22:29:57 +0500

    working frontend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from backend.routers import auth, moderate
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For development: allow all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(moderate.router, prefix="/moderate", tags=["Moderation"])
